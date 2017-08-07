12991e31f2f08df565c800202e83c2f7cd610fdf
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-08-07 10:41:21 -0700

    cmd/tmxrc: scan the entire code

--- cmd/tmxrc/main.go
+++ cmd/tmxrc/main.go
@@ -1,25 +1,46 @@
 package main
 
-import "flag"
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+	"path"
+	"regexp"
+
+	"github.com/kalbasit/tmx"
+)
 
 var (
-	// profiles represents the profile we are scanning
-	profile string
+	// codePath is the path to the code folder
+	codePath string
 
-	// workspace is the workspace we are scanning
-	workspace string
+	// cachePath is the absolute path of the cache file
+	cachePath string
 )
 
 func init() {
-	flag.StringVar(&profile, "profile", "", "select the profile to scan. Default to all profiles")
-	flag.StringVar(&workspace, "workspace", "", "select the workspace to scan. Default to all workspaces")
+	flag.StringVar(&codePath, "code", path.Join(os.Getenv("HOME"), "code"), "The code path to scan")
+	flag.StringVar(&cachePath, "cache", path.Join(os.Getenv("HOME"), ".cache", "tmx.json"), "The path of the file used to store the cache")
 }
 
 func main() {
+	// parse the flags
 	flag.Parse()
-
-}
-
-func scan(dir string) {
-
+	// create the code and scan
+	c := &tmx.Code{
+		Path:           codePath,
+		ExcludePattern: regexp.MustCompile("^.snapshots$"),
+	}
+	c.Scan()
+	// open the file
+	f, err := os.Create(cachePath)
+	if err != nil {
+		log.Fatalf("error opening the cache file %q: %s", cachePath, err)
+	}
+	defer f.Close()
+	// marshal it into JSON
+	if err := json.NewEncoder(f).Encode(c); err != nil {
+		log.Fatalf("error saving code as JSON at %q: %s", codePath, err)
+	}
 }
