3ff3609bb2254c02f7442e316dc552bd9d787f73
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-08-06 21:38:04 -0700

    test: helper function

---
+++ util_test.go
@@ -0,0 +1,13 @@
+package tmx
+
+func prepareFilesystem() {
+	// TestWorkspacePath
+	AppFs.MkdirAll("/home/kalbasit/code/TestWorkspaceScan/base/src/github.com/kalbasit/tmx/.git", 0755)
+	AppFs.MkdirAll("/home/kalbasit/code/TestWorkspaceScan/base/src/github.com/kalbasit/dotfiles/.git", 0755)
+	AppFs.MkdirAll("/home/kalbasit/code/TestWorkspaceScan/STORY-123/src/github.com/kalbasit/private/.git", 0755)
+
+	// TestProfilePath
+	AppFs.MkdirAll("/home/kalbasit/code/TestProfileScan/base/src/github.com/kalbasit/tmx/.git", 0755)
+	AppFs.MkdirAll("/home/kalbasit/code/TestProfileScan/base/src/github.com/kalbasit/dotfiles/.git", 0755)
+	AppFs.MkdirAll("/home/kalbasit/code/TestProfileScan/STORY-123/src/github.com/kalbasit/private/.git", 0755)
+}
