c5fcb285c54db2268bd7ef47774701594a172229
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-08-09 18:01:55 -0700

    cmd/tmxrc: use Code.Save to generate the cache

--- cmd/tmxrc/main.go
+++ cmd/tmxrc/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"log"
 	"os"
@@ -14,33 +13,21 @@ import (
 var (
 	// codePath is the path to the code folder
 	codePath string
-
-	// cachePath is the absolute path of the cache file
-	cachePath string
 )
 
 func init() {
 	flag.StringVar(&codePath, "code", path.Join(os.Getenv("HOME"), "code"), "The code path to scan")
-	flag.StringVar(&cachePath, "cache", path.Join(os.Getenv("HOME"), ".cache", "tmx.json"), "The path of the file used to store the cache")
 }
 
 func main() {
 	// parse the flags
 	flag.Parse()
 	// create the code and scan
-	c := &tmx.Code{
-		Path:           codePath,
-		ExcludePattern: regexp.MustCompile("^.snapshots$"),
-	}
+	c := tmx.New(codePath, regexp.MustCompile("^.snapshots$"))
+	// scan the code folder
 	c.Scan()
-	// open the file
-	f, err := os.Create(cachePath)
-	if err != nil {
-		log.Fatalf("error opening the cache file %q: %s", cachePath, err)
-	}
-	defer f.Close()
-	// marshal it into JSON
-	if err := json.NewEncoder(f).Encode(c); err != nil {
-		log.Fatalf("error saving code as JSON at %q: %s", codePath, err)
+	// save it
+	if err := c.Save(); err != nil {
+		log.Fatalf("error saving the code to the cache file: %s", err)
 	}
 }
