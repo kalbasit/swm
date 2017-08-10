6d45f7974b3335f1dc8512503be2f0bf5478a0d1
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-08-09 21:35:50 -0700

    make workspace and profile optional

--- cmd/tmx/main.go
+++ cmd/tmx/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -24,6 +25,9 @@ var (
 	// profile select the profile, defaults to the current profile
 	profile string
 
+	// socketPath is the full path of the workspace
+	socketPath string
+
 	// select the workspace, defaults to the current workspace
 	workspace string
 
@@ -41,7 +45,8 @@ func init() {
 	flag.StringVar(&codePath, "code", path.Join(os.Getenv("HOME"), "code"), "The code path to scan")
 	flag.BoolVar(&killPane, "kill-pane", false, "kill the pane after the session has been switched; only relevant within TMUX")
 	flag.StringVar(&profile, "profile", os.Getenv("ACTIVE_PROFILE"), "the name of the profile")
-	flag.StringVar(&workspace, "workspace", parseWorkspace(), "the name of the workspace")
+	flag.StringVar(&workspace, "workspace", "", "the name of the workspace")
+	flag.StringVar(&socketPath, "socket-path", os.Getenv("TMUX"), "the path to the socket name")
 
 	var err error
 	fzfPath, err = exec.LookPath("fzf")
@@ -61,15 +66,14 @@ func init() {
 func main() {
 	// parse the flags
 	flag.Parse()
-	// start the TMUX server
-	cmd := exec.Command(tmuxPath, "-L", workspace, "start-server")
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("error running %q: %s", fmt.Sprintf("%s -L %s start-server", tmuxPath, workspace), err)
+	// parse the socketPath as workspace if needed
+	if workspace == "" && socketPath != "" {
+		workspace = strings.Split(socketPath, ",")[0]
 	}
 	// create the code and load/scan
 	c := tmx.New(codePath, regexp.MustCompile("^.snapshots$"))
 	if err := c.LoadOrScan(); err != nil {
-		log.Fatalf("error loading the code, did you run tmxrc?: %s", err)
+		log.Fatalf("error loading the code: %s", err)
 	}
 	// select the session using fzf
 	sessionName, err := withFilter(fzfPath, func(stdin io.WriteCloser) {
@@ -95,22 +99,20 @@ func tmuxStart(c *tmx.Code, sessions []string) error {
 		if err != nil {
 			return err
 		}
-		// run tmux ls -F #{session_name} to find out if we have one running already
-		cmd := exec.Command(tmuxPath, "-L", workspace, "list-sessions", "-F", "#{session_name}")
-		result, err := cmd.Output()
-		if err != nil || !regexp.MustCompile("^"+sessionName+"$").Match(result) {
+		// run tmux has-session -t sessionName to check if session already exists
+		if err := exec.Command(tmuxPath, "-L", project.WorkspaceName, "has-session", "-t", sessionName).Run(); err != nil {
 			// session does not exist, we should start it
 			for _, args := range [][]string{
 				// start the session
-				{"-L", workspace, "new-session", "-d", "-s", sessionName},
+				{"-L", project.WorkspaceName, "new-session", "-d", "-s", sessionName},
 				// set the active profile
-				{"-L", workspace, "set-environment", "-t", sessionName, "ACTIVE_PROFILE", profile},
+				{"-L", project.WorkspaceName, "set-environment", "-t", sessionName, "ACTIVE_PROFILE", project.ProfileName},
 				// set the new GOPATH
-				{"-L", workspace, "set-environment", "-t", sessionName, "GOPATH", path.Join(c.Path, profile, workspace)},
+				{"-L", project.WorkspaceName, "set-environment", "-t", sessionName, "GOPATH", path.Join(c.Path, project.ProfileName, project.WorkspaceName)},
 				// start a new shell on window 1
-				{"-L", workspace, "new-window", "-t", sessionName + ":1"},
+				{"-L", project.WorkspaceName, "new-window", "-t", sessionName + ":1"},
 				// start vim in the first window
-				{"-L", workspace, "send-keys", "-t", sessionName + ":0", "clear; vim", "Enter"},
+				{"-L", project.WorkspaceName, "send-keys", "-t", sessionName + ":0", "clear; vim", "Enter"},
 			} {
 				cmd := exec.Command(tmuxPath, args...)
 				cmd.Dir = project.Path()
@@ -121,11 +123,24 @@ func tmuxStart(c *tmx.Code, sessions []string) error {
 		}
 		// attach the last session
 		if i == len(sessions)-1 {
-			fmt.Printf("must attach now\n")
+			if os.Getenv("TMUX") != "" {
+				// kill the pane once attached
+				defer func() {
+					if killPane {
+						exec.Command(tmuxPath, "-L", project.WorkspaceName, "kill-pane").Run()
+					}
+				}()
+				return exec.Command(tmuxPath, "-L", project.WorkspaceName, "switch-client", "-t", sessionName).Run()
+			}
+			cmd := exec.Command(tmuxPath, "-L", project.WorkspaceName, "attach", "-t", sessionName)
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			return cmd.Run()
 		}
 	}
 
-	return nil
+	return errors.New("no session selected")
 }
 
 func findProject(c *tmx.Code, name string) (*tmx.Project, error) {
@@ -136,7 +151,7 @@ func findProject(c *tmx.Code, name string) (*tmx.Project, error) {
 	}
 	// if the returned project belongs to the base workspace and we are not on
 	// a base workspace then we have to clone it, and update the cache.
-	if workspace != tmx.BaseWorkspaceName && p.Base() {
+	if workspace != "" && workspace != tmx.BaseWorkspaceName && p.Base() {
 		// deep clone p into p2 and change the workspace
 		p2 := &tmx.Project{}
 		*p2 = *p
@@ -180,7 +195,7 @@ func getSessionNames(c *tmx.Code) []string {
 	if w != nil {
 		return append(w.SessionNames(), p.BaseWorkspace().SessionNames()...)
 	}
-	return w.SessionNames()
+	return p.BaseWorkspace().SessionNames()
 }
 
 // withFilter filters input using fzf
@@ -205,9 +220,4 @@ func withFilter(command string, input func(in io.WriteCloser)) ([]string, error)
 		res = res[0 : len(res)-1]
 	}
 	return res, nil
-}
-
-func parseWorkspace() string {
-	// TODO: parse os.Getenv("TMUX")
-	return "base"
 }
