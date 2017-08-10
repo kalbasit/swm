ff2ff6c90e6d5c137caf0723f3568417862601de
Wael Nasreddine <wael.nasreddine@gmail.com>
2017-08-09 20:44:27 -0700

    workspace: assert worktrees are indexed (#4)

--- util_test.go
+++ util_test.go
@@ -7,7 +7,12 @@ func prepareFilesystem(test string) {
 	case "TestWorkspaceScan":
 		AppFs.MkdirAll("/home/kalbasit/code/TestWorkspaceScan/base/src/github.com/kalbasit/tmx/.git", 0755)
 		AppFs.MkdirAll("/home/kalbasit/code/TestWorkspaceScan/base/src/github.com/kalbasit/dotfiles/.git", 0755)
-		AppFs.MkdirAll("/home/kalbasit/code/TestWorkspaceScan/STORY-123/src/github.com/kalbasit/private/.git", 0755)
+
+		AppFs.MkdirAll("/home/kalbasit/code/TestWorkspaceScan/STORY-123/src/github.com/kalbasit/dotfiles", 0755)
+		afero.WriteFile(AppFs, "/home/kalbasit/code/TestWorkspaceScan/STORY-123/src/github.com/kalbasit/dotfiles/.git", []byte(
+			"gitdir: /home/kalbasit/code/TestWorkspaceScan/base/src/github.com/kalbasit/.git/worktrees/dotfiles",
+		), 0644)
+
 		AppFs.MkdirAll("/home/kalbasit/code/TestWorkspaceScan/invalid/repo/.git", 0755)
 	case "TestProfileScan":
 		AppFs.MkdirAll("/home/kalbasit/code/TestProfileScan/base/src/github.com/kalbasit/tmx/.git", 0755)
