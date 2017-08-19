797ddb286b81a3c8a1d6a0661001e897dc491661
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-08-19 15:17:24 -0700

    move flags up to be global

--- cmd/swm/main.go
+++ cmd/swm/main.go
@@ -19,6 +19,13 @@ func main() {
 		Name:    "swm",
 		Version: version,
 		Usage:   "swm <command>",
+		Flags: []cli.Flag{
+			&cli.StringFlag{Name: "profile", Usage: "The profile for the TMUX session", Value: getDefaultProfile()},
+			&cli.StringFlag{Name: "story", Usage: "The story for the TMUX session", Value: getDefaultStory()},
+			&cli.StringFlag{Name: "code-path", Usage: "The absolute path to the code path", Value: path.Join(os.Getenv("HOME"), "code")},
+			&cli.StringFlag{Name: "ignore-pattern", Usage: "The Regex pattern to ignore", Value: "^.snapshots$"},
+			&cli.BoolFlag{Name: "debug", Usage: "enable debug mode"},
+		},
 		Authors: []*cli.Author{
 			{
 				Name:  "Wael Nasreddine",
@@ -29,12 +36,6 @@ func main() {
 			// tmux for switch client
 			{
 				Name: "tmux",
-				Flags: []cli.Flag{
-					&cli.StringFlag{Name: "profile", Usage: "The profile for the TMUX session", Value: getDefaultProfile()},
-					&cli.StringFlag{Name: "story", Usage: "The story for the TMUX session", Value: getDefaultStory()},
-					&cli.StringFlag{Name: "code-path", Usage: "The absolute path to the code path", Value: path.Join(os.Getenv("HOME"), "code")},
-					&cli.StringFlag{Name: "ignore-pattern", Usage: "The Regex pattern to ignore", Value: "^.snapshots$"},
-				},
 				Subcommands: []*cli.Command{
 					// switch client switches tmux client
 					{
