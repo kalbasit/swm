b0ba0d9bf880f1c2e6bb4503b9a14062d499ecbf
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-27 11:52:09 -0700

    swm: change the default code-path to parse the env var CODE_PATH

--- cmd/swm/main.go
+++ cmd/swm/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"path"
 	"sort"
 
 	"github.com/rs/zerolog/log"
@@ -18,7 +17,7 @@ var app = &cli.App{
 	Flags: []cli.Flag{
 		&cli.StringFlag{Name: "profile", Usage: "The profile for the TMUX session", Value: getDefaultProfile()},
 		&cli.StringFlag{Name: "story", Usage: "The story for the TMUX session", Value: getDefaultStory()},
-		&cli.StringFlag{Name: "code-path", Usage: "The absolute path to the code path", Value: path.Join(os.Getenv("HOME"), "code")},
+		&cli.StringFlag{Name: "code-path", Usage: "The absolute path to the code path", Value: os.Getenv("CODE_PATH")},
 		&cli.StringFlag{Name: "ignore-pattern", Usage: "The Regex pattern to ignore", Value: "^.snapshots$"},
 		&cli.BoolFlag{Name: "debug", Usage: "enable debug mode"},
 	},
