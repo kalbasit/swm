7377cc2af0df32f60c6b2fd2dfc95718a7998246
Wael Nasreddine <wael.nasreddine@gmail.com>
2019-06-04 22:45:10 -0700

    tmux/switch_client: remove any slashes from the socket name (#84)
    If the profile or the story contains a slash TMux returns an error, but it also crashes the system with a fork bomb.
    
    close #83

--- tmux/tmux_test.go
+++ tmux/tmux_test.go
@@ -2,12 +2,43 @@ package tmux
 
 import (
 	"io/ioutil"
+	"testing"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/stretchr/testify/assert"
 )
 
 func init() {
 	// discard logs
 	log.Logger = zerolog.New(ioutil.Discard)
+}
+
+func TestSocketName(t *testing.T) {
+	t.Run("typical profile/story", func(t *testing.T) {
+		tmx := &tmux{options: &Options{
+			Profile: "personal",
+			Story:   "STORY-123",
+		}}
+
+		assert.Equal(t, "personal@STORY-123", tmx.socketName())
+	})
+
+	t.Run("profile with a slash", func(t *testing.T) {
+		tmx := &tmux{options: &Options{
+			Profile: "personal/a",
+			Story:   "STORY-123",
+		}}
+
+		assert.Equal(t, "personal_a@STORY-123", tmx.socketName())
+	})
+
+	t.Run("story with a slash", func(t *testing.T) {
+		tmx := &tmux{options: &Options{
+			Profile: "personal",
+			Story:   "feature/STORY-123",
+		}}
+
+		assert.Equal(t, "personal@feature_STORY-123", tmx.socketName())
+	})
 }
