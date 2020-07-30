c837b0e1f3f92d76d2e69420f428150f84d624d1
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2020-07-29 17:01:22 -0700

    fix ssl in the shell

--- shell.nix
+++ shell.nix
@@ -18,4 +18,27 @@ nixpkgs.mkShell {
 
   GOPATH = "${home}/.cache/go";
   GO111MODULE = "on";
+
+  shellHook = builtins.concatStringsSep "\n" [
+    # the shell hook is exporting an invalid cert file. Get rid of this cert
+    # file if it does not exist.
+    ''
+      if [ -n "''${SSL_CERT_FILE:-}" ] || [ -n "''${NIX_SSL_CERT_FILE:-}" ]; then
+        local ca=""
+        if [ -n "''${SSL_CERT_FILE:-}" ] && [ -f "$SSL_CERT_FILE" ]; then
+          ca="$SSL_CERT_FILE"
+        elif [ -n "''${NIX_SSL_CERT_FILE:-}" ] && [ -f "$NIX_SSL_CERT_FILE" ]; then
+          ca="$NIX_SSL_CERT_FILE"
+        elif [ -f"/etc/ssl/certs/ca-bundle.crt" ]; then
+          ca="/etc/ssl/certs/ca-bundle.crt"
+        fi
+        if [ -f "$ca" ]; then
+            export SSL_CERT_FILE="$ca"
+            export NIX_SSL_CERT_FILE="$ca"
+          fi
+      else
+        unset SSL_CERT_FILE NIX_SSL_CERT_FILE
+      fi
+    ''
+  ];
 }
