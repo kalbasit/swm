b9bcf38e60b294e9868b4a5003b791d24dc97684
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2020-08-02 18:47:53 -0700

    cmd: print an error about the config file only if an err is returned

--- cmd/root.go
+++ cmd/root.go
@@ -101,7 +101,7 @@ func initConfig() {
 	viper.SetConfigFile(viper.GetString("config"))
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil && !os.IsNotExist(err) {
+		panic(errors.Wrap(err, "error reading the config file"))
 	}
 }
