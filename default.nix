e51afc610eac0a54f881bd2fc10b5becf52b1420
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2020-08-02 18:24:50 -0700

    build local package only

--- default.nix
+++ default.nix
@@ -15,6 +15,8 @@ buildGoModule rec {
 
   nativeBuildInputs = [ fzf git tmux procps installShellFiles ];
 
+  subPackages = [ "." ];
+
   postInstall = ''
     for shell in bash zsh fish; do
       $out/bin/swm auto-complete $shell > swm.$shell
