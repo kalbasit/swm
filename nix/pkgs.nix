7c0f12ad43bc54aa019d1896f837dd117a3bd606
Wael Nasreddine <wael.nasreddine@gmail.com>
2020-12-16 21:52:57 -0800

    nix: update nixpkgs (#93)

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs-channels/archive/840c782d507d60aaa49aa9e3f6d0b0e780912742.tar.gz";
-  sha256 = "14q3kvnmgz19pgwyq52gxx0cs90ddf24pnplmq33pdddbb6c51zn";
+  url = "https://github.com/NixOS/nixpkgs/archive/f02bf8ffb9a5ec5e8f6f66f1e5544fd2aa1a0693.tar.gz";
+  sha256 = "156f48manydh8sbrs8j97mzvwdz2zaw51xlm0grrrnns5wa5aaln";
 }
